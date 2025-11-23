f6b993a2b631765bc9e20f77ced4c467832e8247
SONY VAIO GUI <worktradestudygrra@gmail.com>
2025-11-22 22:06:49 -0300

    Inicialização da challenge + app core + models e serializers

---
+++ challenge_backend/core/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Trainer(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Pokemon(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.URLField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class TrainerPokemon(models.Model):
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('trainer', 'pokemon')
+                                           
+
+    def __str__(self):
+        return f"{self.trainer.name} -> {self.pokemon.name}"
